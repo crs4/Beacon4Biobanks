a26c48f429eab62dd630a50b16fe092d96283f08
asulis <alessandro.sulis@crs4.it>
2024-05-06 11:34:57 +0200

    feat: Adds the countType metadata in both /individual and /biosample responses

--- beacon/response/build_response.py
+++ beacon/response/build_response.py
@@ -155,6 +155,9 @@ def build_beacon_count_response(data, num_total_results, qparams: RequestParams,
     """
     Transform data into the Beacon response format.
     """
+    print('********')
+    print(entity_schema)
+    print('********')
     beacon_response = {
         'meta': build_meta(qparams, entity_schema, Granularity.COUNT),
         'responseSummary': build_response_summary(num_total_results > 0, num_total_results),
@@ -169,7 +172,9 @@ def build_beacon_count_response(data, num_total_results, qparams: RequestParams,
                     "resultCountDescription": {
                         "minRange": num_total_results,
                         "maxRange": num_total_results
-                    }
+                    },
+                    "countType": "Overall number of{0}Biosamples hosted into one or more Biobanks in BBMRI Directory".format(
+                        " Individuals related to " if entity_schema['entityType'] == 'individual' else " ")
                 }
             }
             ]
